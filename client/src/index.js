cba72fc2e182abbbc7d61a0a9f74e08317e59651
reettik <reettikgoswami1997@gmail.com>
2020-05-20 12:16:39 +0530

    add redux dev-tool

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import  { Provider } from "react-redux"
-import {createStore} from "redux";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk"
 
 import App from "./component/App";
 import reducer from "./reducers/index";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
- <Provider  store = {createStore(reducer)}>
-   <App/>
- </Provider>
-, document.querySelector("#root"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
