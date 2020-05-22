a59558b96e09bad7cfb383cc45a631268e37908a
reettik <reettikgoswami1997@gmail.com>
2020-05-22 23:04:20 +0530

    stream reducer done

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,64 @@
-import {SIGN_IN , SIGN_OUT} from "./types"
- 
-export const signIn = (userId)=>{
-   return {
-     type : SIGN_IN,
-     payload : userId
-   };
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+} from "./types";
+import streams from "../api/streams";
+
+export const signIn = (userId) => {
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValue) => async (dispatch) => {
+  const responce = await streams.post("/streams", formValue);
+  dispatch({
+    type: CREATE_STREAM,
+    payload: responce.data,
+  });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const responce = await streams.get("/streams",);
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: responce.data,
+  });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const responce = await streams.post(`/streams/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: responce.data,
+  });
+};
+
+export const editStream  = (id , formValue) => async dispatch =>{
+  const responce = await streams.put(`/streams/${id}` , formValue);
+  dispatch({
+     type : EDIT_STREAM,
+     payload : responce.data
+  })
 }
 
-export const signOut = ()=>{
-  return {
-    type : SIGN_OUT
-  };
+export const deleteStream  = (id) => async dispatch =>{
+  await streams.delete(`/streams/${id}`);
+  dispatch({
+     type : DELETE_STREAM,
+     payload : id
+  })
 }
+
--- client/src/component/streams/StreamCreate.jsx
+++ client/src/component/streams/StreamCreate.jsx
@@ -1,11 +1,83 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import {createStream} from "../../actions/index";
 
-const StreamCreate = (props) => {
-  return (
-    <div>
-      StreamCreate
-    </div>
-  );
+
+class StreamCreate extends Component {
+  renderError = ({ error  , touched}) => {
+    // console.log(touched , error);
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header"> {error}</div>
+        </div>
+      );
+    }else{
+      return null;
+    }
+  };
+
+  renderInput = (fromProps) => {
+    // console.log(fromProps);
+    const className = `field ${fromProps.meta.touched && fromProps.meta.error ? "error" : ""}`
+    return (
+      <div className={className}>
+        <label> {fromProps.label}</label>
+        <input {...fromProps.input} autoComplete="off" />
+        {this.renderError(fromProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (fromValues) => {
+    // console.log(fromValues);
+     this.props.createStream(fromValues);
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="decription"
+          component={this.renderInput}
+          label="Enter Decription"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+const validate = (fromValues) => {
+  const error = {};
+  if (!fromValues.title) {
+    error.title = "You must enter a title";
+  }
+  if (!fromValues.decription) {
+    error.decription = "you must enter a decription";
+  }
+  return error;
+};
+
+
+//  Bad way
+// export default connect(null , {createStream})(reduxForm({
+//   form: "streamCreate",
+//   validate: validate,
+// })(StreamCreate));
+
+
+// Better way 
+const fromWrapped = reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(StreamCreate)
+
+export default connect(null , {createStream})(fromWrapped);
+
